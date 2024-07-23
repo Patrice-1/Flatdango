0b155befc3bcf4dd77d112a843cbdb64449e6209
Patrick <njorogepatrick739@gmail.com>
2024-07-23 18:02:40 +0300

    submit

--- public/script.js
+++ public/script.js
@@ -6,7 +6,13 @@ fetch("http://localhost:3000/films")
         data.forEach((data)=>{
         console.log(data);
         });
+
+        
     });
 
+    const element = document.getElementById(data);
+    const navBtn = document.getElementById("nav-btn");
+    const homeBtn = document.getElementById("home-btn");
+    const moviesBtn = document.getElementById("movies-btn");
+    console.log(data);
 
-    
